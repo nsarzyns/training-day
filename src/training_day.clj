d004b11bc43ffe8f90c04c5bc546b06747d2138e
nsarzyns <nsarzyns@ycp.edu>
2017-10-26 21:49:12 -0400

    Completed

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,11 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
-(def hai "O HAI!")
+(def hai "O HAddI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (* 1/2 (+ a b)))
